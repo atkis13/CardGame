b25e0bc9b5de216f35f47a49b26195c4a39fb5dc
atkis13 <kis_attila_1349@yahoo.com>
2020-04-06 21:48:50 +0300

    initial commit

---
+++ Deck.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace CardGame
+{
+    [XmlRoot("Deck")]
+    class Deck
+    {
+        [XmlElement("Card")]
+        public List<Card> cards { get; set; }
+    }
+
+    public class Card
+    {
+
+        public string name { get; set; }
+
+        public int strenght { get; set; }
+
+
+    }
+}
--- MainForm.cs
+++ MainForm.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace CardGame
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
